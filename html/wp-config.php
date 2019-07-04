0fb3e83d066ff3c3cd1d59e7f53e0ee27ccaf00d
kaotu <patthraporn.tu@mail.kmutt.ac.th>
2019-07-04 14:59:46 +0700

    Change config file from sample file to wp-config.php :new_moon_with_face:

--- html/wp-config.php
+++ html/wp-config.php
@@ -17,19 +17,26 @@
  *
  * @package WordPress
  */
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'exampledb');
+define( 'DB_NAME', 'database_name_here' );
+
 /** MySQL database username */
-define( 'DB_USER', 'exampleuser');
+define( 'DB_USER', 'username_here' );
+
 /** MySQL database password */
-define( 'DB_PASSWORD', 'examplepass');
+define( 'DB_PASSWORD', 'password_here' );
+
 /** MySQL hostname */
-define( 'DB_HOST', 'db');
+define( 'DB_HOST', 'localhost' );
+
 /** Database Charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8');
+define( 'DB_CHARSET', 'utf8' );
+
 /** The Database Collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '');
+define( 'DB_COLLATE', '' );
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
@@ -39,15 +46,17 @@ define( 'DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'f222a8d067ad19eb7ce1dc755e30c04ef53a40b1');
-define( 'SECURE_AUTH_KEY',  '25f93e9cf6ab0559ab578671cd80d5968f42ea70');
-define( 'LOGGED_IN_KEY',    '681062147a42e44631f56e14c68e49c494164a3c');
-define( 'NONCE_KEY',        '6069d1052bf9aaf9e260ce7de8dc05817d26766e');
-define( 'AUTH_SALT',        '39bb0b372c6d313509886c092ebc2bf9dc130ad9');
-define( 'SECURE_AUTH_SALT', '6d8fe1cd57f9fb56bb604cc8224f114025d729b1');
-define( 'LOGGED_IN_SALT',   'c215851164bf3127527cd416849091c05a3f6305');
-define( 'NONCE_SALT',       '68b9ded6dd08cddded9a664df546a81d76ea8c09');
+define( 'AUTH_KEY',         'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+define( 'NONCE_SALT',       'put your unique phrase here' );
+
 /**#@-*/
+
 /**
  * WordPress Database Table prefix.
  *
@@ -55,6 +64,7 @@ define( 'NONCE_SALT',       '68b9ded6dd08cddded9a664df546a81d76ea8c09');
  * a unique prefix. Only numbers, letters, and underscores please!
  */
 $table_prefix = 'wp_';
+
 /**
  * For developers: WordPress debugging mode.
  *
@@ -68,23 +78,13 @@ $table_prefix = 'wp_';
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
 define( 'WP_DEBUG', false );
-// If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
-// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-	$_SERVER['HTTPS'] = 'on';
-}
-/* That's all, stop editing! Happy publishing. */
-define( 'WP_ALLOW_MULTISITE', true );
-define('MULTISITE', true);
-define('SUBDOMAIN_INSTALL', false);
-define('DOMAIN_CURRENT_SITE', 'khoto.tk');
-define('PATH_CURRENT_SITE', '/');
-define('SITE_ID_CURRENT_SITE', 1);
-define('BLOG_ID_CURRENT_SITE', 1);
 
+/* That's all, stop editing! Happy publishing. */
+define( 'WP_ALLOW_MULTISITE', true );$
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
 }
+
 /** Sets up WordPress vars and included files. */
 require_once( ABSPATH . 'wp-settings.php' );
