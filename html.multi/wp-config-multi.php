6765cb6a3ddd6818fac8c0648d6fe6f251a1aeb0
kaotu <patthraporn.tu@mail.kmutt.ac.th>
2019-07-13 16:22:08 +0700

    Change line cookie block :new_moon_with_face:

--- html.multi/wp-config-multi.php
+++ html.multi/wp-config-multi.php
@@ -73,8 +73,7 @@ define( 'WP_DEBUG', false );
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
 	$_SERVER['HTTPS'] = 'on';
 }
-define('COOKIE_DOMAIN', $_SERVER['HTTP_HOST'] );
-
+define('COOKIE_DOMAIN', false);
 /* That's all, stop editing! Happy publishing. */
 define( 'WP_ALLOW_MULTISITE', true );
 define('MULTISITE', true);
